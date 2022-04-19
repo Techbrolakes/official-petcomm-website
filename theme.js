0a795dfc69b411baec774ef13e8b270ce514f8e7
Lekan Dar <Lekandar11@gmail.com>
2022-04-18 21:48:54 -1200

    version-1.0

---
+++ theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  textStyles: {
+    h1: {
+      fontSize: ["50px", "80px", "90px", "100px"],
+    },
+  },
+});
+
+export default theme;
